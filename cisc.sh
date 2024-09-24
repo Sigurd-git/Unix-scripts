9eb1e63c040bb86cf574bdb0d2cc0053fba95b04
GUOYANG LIAO <guoyang_liao@urmc.rochester.edu>
2024-09-24 12:11:05 -0400

    * chore(cisc.sh): refactor cisc.sh script to support different connection methods * - Add support for specifying connection method through command line argument * - Prompt user for secondary password if connection method is sms

--- cisc.sh
+++ cisc.sh
@@ -1,3 +1,16 @@
 current_path="$(dirname "$0")"
 source $current_path/read_user_password.sh
-printf "$USER\n$PASSWORD\npush\ny" | /opt/cisco/anyconnect/bin/vpn -s connect vpn.rochester.edu 
+method="push" # Default method is push
+if [ "$1" == "--method" ] && [ -n "$2" ]; then
+    method=$2
+fi
+
+# Connect to VPN with the specified method
+printf "$USER\n$PASSWORD\n$method\ny" | /opt/cisco/anyconnect/bin/vpn -s connect vpn.rochester.edu 
+
+# If the method is sms, connect again and input secondary password
+if [ "$method" == "sms" ]; then
+    # Prompt the user to manually input the secondary password
+    read -s -p "Enter secondary password: " SECONDARY_PASSWORD
+    printf "$USER\n$PASSWORD\n$SECONDARY_PASSWORD\n" | /opt/cisco/anyconnect/bin/vpn -s connect vpn.rochester.edu 
+fi
